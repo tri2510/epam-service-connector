10986d416aa7f16b4fabd5a25beac998e0adb33f
tri2510 <tri2510@gmail.com>
2025-06-30 23:01:21 +0700

    Add MIT license headers and LICENSE file

--- run.sh
+++ run.sh
@@ -1,3 +1,11 @@
+# Copyright (c) 2025 Eclipse Foundation.
+# 
+# This program and the accompanying materials are made available under the
+# terms of the MIT License which is available at
+# https://opensource.org/licenses/MIT.
+#
+# SPDX-License-Identifier: MIT
+
 # Prepare environment variable for KUKSA
 #export KUKSA_DATABROKER_METADATA_FILE=/home/root/vss.json
 
