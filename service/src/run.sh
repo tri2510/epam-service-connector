b209519d6c16578e8a9c5ccfaf6868d6fb9692eb
NhanLuongBGSV <nhan.luongnguyen@vn.bosch.com>
2025-08-26 16:27:33 +0700

    Update start script

--- service/src/run.sh
+++ service/src/run.sh
@@ -1,12 +1,5 @@
-# Copyright (c) 2025 Eclipse Foundation.
-# 
-# This program and the accompanying materials are made available under the
-# terms of the MIT License which is available at
-# https://opensource.org/licenses/MIT.
-#
-# SPDX-License-Identifier: MIT
+#!/bin/bash
 
 ifconfig
-
 cd app
 /usr/bin/python3 -u syncer.py
